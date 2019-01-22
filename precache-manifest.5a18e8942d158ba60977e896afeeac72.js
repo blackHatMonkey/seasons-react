b756f75e2866b5563dd1573e439d181f157eb2ea
Brian Smith <brian.smith@riseup.net>
2019-01-22 17:16:47 -0500

    Updates

---
+++ precache-manifest.5a18e8942d158ba60977e896afeeac72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a5a2d821b25b312d3c8",
+    "url": "/static/css/main.ae4f4eeb.chunk.css"
+  },
+  {
+    "revision": "1a5a2d821b25b312d3c8",
+    "url": "/static/js/main.1a5a2d82.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1730da6f420b7d7547ba9f4752b01584",
+    "url": "/index.html"
+  }
+];
